f3c2e07f57499fe1fb6a285258600e6b82e5ff9f
Christopher Wong <chriskwong91@gmail.com>
2016-09-07 16:44:25 -0700

    passport logic to create new user

---
+++ server/config/passport.js
@@ -0,0 +1,58 @@
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../users/user.js');
+
+module.exports = function(passport) {
+
+  //passport session setup for persistent login session
+
+  // serialize user for session
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  // deserialize the user
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+  // Set-up for local strategy signup
+  passport.use('local-signup', new LocalStrategy({
+    usernameField : 'username',
+    passwordfield: 'password',
+    passReqToCallback : true,
+  },
+  function(req, username, password, done) {
+
+    // needed for User.findOne to work
+    process.nextTick(function() {
+
+      User.findone({ 'local.username' : username }, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+
+        // check if username exists
+        if (user) {
+          return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+        } else {
+
+          // no user with that username, create user
+          var newUser = new User();
+
+          //set up credentials
+          newUser.local.username = username;
+          newUser.local.password = newUser.generateHash(password);
+
+          newUser.save(function(err) {
+            if(err) {
+              throw err;
+            }
+            return done(null, newUser);
+          });
+        }
+      });
+    });
+  }));
+};
