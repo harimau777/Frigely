e2f6d63b03464d3ea8c50d19afab8acb2c1dc635
Christopher Wong <chriskwong91@gmail.com>
2016-09-06 19:45:07 -0700

    Added recipes array and a get Recipes function

--- client/js/services/services.js
+++ client/js/services/services.js
@@ -1,25 +1,37 @@
 angular.module('fridgely.services', [])
   .factory('Search', function($http) {
+    var recipes = [];
 
     /**
      * @name sendIngredients
-     * @desc Given a list of ingredients, send those ingredients to our server, and get a list 
-     *   of recipes as a response. 
-     * @param {???} ingredients - A list of ingredients to post to the server. 
-     * @returns {Promise.???} Return a promise containing a list of ingredientes. 
+     * @desc Given a list of ingredients, send those ingredients to our server, and get a list
+     *   of recipes as a response.
+     * @param {???} ingredients - A list of ingredients to post to the server.
+     * @returns {Promise.???} Return a promise containing a list of ingredientes.
      */
     var sendIngredients = function(ingredients) {
       return $http({
         method: 'POST',
         url: '/api/recipes',
-        data: ingredients 
+        data: ingredients
       }).then(function(res) {
         return res.data;
       });
     };
 
-    
+    /**
+     * @name getRecipes
+     * @desc Gives the stored recipes for whoever is asking for it
+     * @returns list of recipes
+     */
+
+     var getRecipes = function() {
+      return recipes;
+     };
+
     return {
-      sendIngredients: sendIngredients
+      sendIngredients: sendIngredients,
+      getRecipes: getRecipes,
+      recipes: recipes
     };
   });
