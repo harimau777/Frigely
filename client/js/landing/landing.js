610843dff25dcdfbccf011eadf2d481500b4dca4
Brian Zhou <bzhou4@gmail.com>
2016-09-07 13:38:56 -0700

    (feature) updated the landing page with new banner image

--- client/js/landing/landing.js
+++ client/js/landing/landing.js
@@ -3,7 +3,7 @@ angular.module('fridgely.landing', [])
     // Title
     $scope.LPtext = {};
     // $scope.LPheader = 'ACT FRIDGE-FRIENDLY. BE FOOD-SAVY. COMBAT SELF-HUNGER.';
-    $scope.LPtext.title = 'FRIDGELY';
+    $scope.LPtext.title = 'Join the Fight Against Self-Hunger';
     $scope.LPtext.descriptions = ['Be food-savy.', 'Act fridge-friendly.', 'Use fridgely.'];
   }
 );
