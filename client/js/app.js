66f8530c117f4d65746106dbb0631960a7ab0a3c
Christopher Wong <chriskwong91@gmail.com>
2016-09-09 18:07:27 -0700

    Added attachTokens factory to add the tokens to header. Also made the routes need to authenticate first

--- client/js/app.js
+++ client/js/app.js
@@ -14,7 +14,6 @@ angular.module('fridgely', [
       .when('/login', {
         templateUrl: 'js/auth/login.html',
         controller: 'AuthController'
-
       })
       .when('/signup', {
         templateUrl: 'js/auth/signup.html',
@@ -22,17 +21,44 @@ angular.module('fridgely', [
       })
       .when('/landing', {
         templateUrl: 'js/landing/landing.html',
-        controller: 'LandingPageController'
+        controller: 'LandingPageController',
+        authenticate: true
       })
       .when('/recipes', {
         templateUrl: 'js/recipes/recipes.html',
-        controller: 'RecipeController'
+        controller: 'RecipeController',
+        authenticate: true
       })
       .when('/search', {
         templateUrl: 'js/search/search.html',
-        controller: 'SearchController'
+        controller: 'SearchController',
+        authenticate: true
       })
       .otherwise({
         redirectTo: '/landing'
       });
+
+      $httpProvider.interceptors.push('AttachTokens');
+  })
+  .factory('AttachTokens', function ($window) {
+    console.log('Attaching Tokens');
+    var attach = {
+      request: function (object) {
+        var jwt = $window.localStorage.getItem('com.fridgely');
+        console.log('jwt: ', jwt);
+        if (jwt) {
+          object.headers['x-access-token'] = jwt;
+        }
+        object.headers['Allow-Control-Allow-Origin'] = '*';
+        return object;
+      }
+    };
+    return attach;
+    })
+  .run(function ($rootScope, $location, Auth) {
+    $rootScope.$on('$routeChangeStart', function( evt, next, current) {
+      if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+        $location.path('/login');
+      }
+    });
   });
