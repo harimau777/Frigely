7742790651c6318e189178c99479cba2b5ca9809
Surender <ilovehackreactor@gmail.com>
2016-09-13 16:24:22 -0700

    made a factory Favorites in client/services/services.js having add, remove and get item from the favorite view

--- client/js/services/services.js
+++ client/js/services/services.js
@@ -36,6 +36,62 @@ angular.module('fridgely.services', [])
       recipes: recipes
     };
   })
+  .factory('Favorites', function($http, $location) {
+    
+    /**
+      * @name addFavorites
+      * @desc Given a list of favorite ingredients and send it to server
+      * @return 
+      */
+    
+    var addFavorite = function(ingredient) {
+      return $http({
+        method: 'POST',
+        url: '/api/favorites',
+        data: ingredient
+      }).then(function() {
+        console.log('you added favorite item');
+      })
+    };
+    
+    /**
+      * @name getFavorites
+      * @desc get all favorites ingredients from the server 
+      * @return  
+      */
+    
+    var getFavorites = function() {
+      return $http({
+        method: 'GET',
+        url: '/api/favorites'
+      }).then(function(resp) {
+        console.log('you requested to get all favorite ingredients: ', resp);
+      })
+
+    };
+
+
+    /**
+      * @name removeFavorites
+      * @desc Iss
+      */ 
+    
+    var removeFavorite = function(noMoreFavItem) {
+      return $http({
+        method: 'DELETE',
+        url: '/api/favorites:id',
+        data: noMoreFavItem
+      }).then(function() {
+        console.log('you deleted all your favorites ingredients...');
+      })
+    };
+
+    return {
+      addFavorite: addFavorite,
+      getFavorites: getFavorites,
+      removeFavorite: removeFavorite
+    }       
+  })
   .factory('Auth', function($http, $location, $window)  {
     /**
      * @name login
@@ -96,4 +152,8 @@ angular.module('fridgely.services', [])
       };
 
   });
-
+// factory fab
+//three function 
+//add fav
+//remove fav
+//get fav
