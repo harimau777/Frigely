2e083502202348fdf48db14d33e6918c11e4c53a
lthai423 <lthai423@gmail.com>
2016-09-16 12:10:09 -0700

    bugs galore

--- client/js/search/search.js
+++ client/js/search/search.js
@@ -3,44 +3,94 @@ angular.module('fridegly.search', [])
     $scope.data = {};
     $scope.shared = Shared;
     $scope.data.ingredients = [];
-    $scope.shared.selected = [];
-    $scope.shared.ingredients = [];
+    $scope.data.selected = [];
+    $scope.data.ingredientList = [];
+    $scope.data.favorites = [];
+
 
     $scope.shared.initIngredients = function() {
-      if ($scope.shared.favorites) {
-        $scope.shared.selected = _.union($scope.data.ingredients, $scope.shared.favorites);
+      if ($scope.data.favorites) {
+        $scope.data.selected = _.union($scope.data.ingredients, $scope.data.favorites);
       }
     };
+
+    var getFavorites = () => {
+      Favorites.getFavorites().then((res) => {
+        $scope.favorites = res.data.local.favorites;
+        console.log($scope.favorites);
+        //$scope.data.favorites = $scope.data.favorites;
+      });
+    };
+    getFavorites();
 
     /**
      * @name addIngredients
      * @desc Takes the user input ingredient and adds it to the array of current ingredients
      * @returns undefined
      */
-    $scope.addIngredient = function () {
+    $scope.addIngredient = function (ingred) {
       // Scrub the input string so that it looks standardized. For exmaple, strings such as
       //   'chICKEN        FinGers' -> 'Chicken Fingers'. 
-      var name = $scope.ingredient.trim().split(/\s+/).map(function(item) {
+      var name = !!ingred ? ingred : $scope.ingredient;
+      name = name.trim().split(/\s+/).map(function(item) {
         return item[0].toUpperCase() + item.substr(1).toLowerCase();
       }).join(' ');
       $scope.data.ingredients.indexOf(name) === -1 && $scope.data.ingredients.push(name);
       $scope.message = '';
-      $scope.shared.selected = _.union($scope.data.ingredients, $scope.shared.favorites);
+      $scope.data.selected = _.union($scope.data.ingredients, $scope.data.favorites);
+      console.log($scope.data.selected);
       $scope.ingredient = '';
-      //$scope.shared.selected = $scope.shared.favorites.concat($scope.data.ingredients);
+      //$scope.data.selected = $scope.data.favorites.concat($scope.data.ingredients);
     };
 
-    $scope.addFavorite = function() {
-      var name = $scope.ingredient.trim().split(/\s+/).map(function(item) {
+    $scope.selectFavorite = (favorite) => {
+      console.log('Clicked');
+        console.log(favorite);
+      if ($scope.data.favorites.indexOf(favorite) === -1) {
+        $scope.data.selected.push(favorite);
+      }
+    };
+
+    $scope.selectIngredient = function(ingredient){
+      $scope.addFavorite(ingredient);
+    }
+
+    $scope.addFavorite = function(favorite) {
+      var name = !!favorite ? favorite : $scope.ingredient;
+      name = name.trim().split(/\s+/).map(function(item) {
         return item[0].toUpperCase() + item.substr(1).toLowerCase();
       }).join(' ');
-      if ($scope.shared.favorites.indexOf(name) === -1) {
-        console.log(name);
+      if ($scope.data.favorites.indexOf(name) === -1) {
+        console.log("name",name);
         Favorites.addFavorite(name).then((resp) => {
-          $scope.shared.getFavorites();
+          getFavorites();
         });
       }
       $scope.ingredient = '';
+    };
+
+    // $scope.addFavorite = () => {
+    //   // used to demo shared object between components.
+    //   $scope.shared.input = $scope.data.favorite;
+    //   console.log('Clicked');
+
+    //   if ($scope.data.favorite && $scope.data.favorites.indexOf($scope.data.favorite) === -1) {
+    //     var name = $scope.data.favorite.trim().split(/\s+/).map(function(item) {
+    //       return item[0].toUpperCase() + item.substr(1).toLowerCase();
+    //     }).join(' ');
+    //     if ($scope.data.favorites.indexOf(name) === -1) {
+    //       Favorites.addFavorite(name).then(() => {
+    //         $scope.data.favorite = '';
+    //         getFavorites();
+    //       });
+    //     }
+    //   }
+    // };
+
+    $scope.removeFavorite = (favorite) => {
+      Favorites.removeFavorite(favorite).then(() => {
+        getFavorites();
+      });
     };
 
     /**
@@ -50,8 +100,7 @@ angular.module('fridegly.search', [])
      * @returns undefined
      */
     $scope.deleteIngredient = function () {
-      $scope.data.ingredients.splice($scope.data.ingredients.indexOf(this.ingredient), 1);
-      $scope.shared.initIngredients();
+      $scope.data.selected.splice($scope.data.ingredients.indexOf(this.ingredient), 1);
     };
 
     /**
@@ -61,11 +110,11 @@ angular.module('fridegly.search', [])
      * @returns undefined
      */
     $scope.sendIngredients = function () {
-      if ($scope.shared.selected.length === 0) {
+      if ($scope.data.selected.length === 0) {
         $scope.message = 'Please add one or more ingredients.';
       } else {
         var ingredients = {
-          ingredients: $scope.shared.selected
+          ingredients: $scope.data.selected
         };
         Search.sendIngredients(ingredients);
       }
@@ -73,8 +122,8 @@ angular.module('fridegly.search', [])
 
     Search.getIngredientList()
       .then(function(res){
-        //$scope.shared.ingredients = res.data.slice(0, 10);
-        $scope.shared.ingredients = res.data;
+        //$scope.data.ingredientList = res.data.slice(0, 10);
+        $scope.data.ingredientList = res.data;
       });
   })
   .component('searchComponent', {
