0bedd9edb60c203343d750dce2da212d3748d11f
abandeali1 <bandeali1@gmail.com>
2016-09-16 13:32:42 -0700

    Merge search and favorites into one component

--- client/js/search/search.js
+++ client/js/search/search.js
@@ -1,91 +1,51 @@
 angular.module('fridegly.search', [])
   .controller('SearchController', function($scope, Search, Shared, Favorites) {
     $scope.data = {};
-    $scope.shared = Shared;
-    $scope.data.ingredients = [];
     $scope.data.selected = [];
     $scope.data.ingredientList = [];
     $scope.data.favorites = [];
 
-
-    $scope.shared.initIngredients = function() {
-      if ($scope.data.favorites) {
-        $scope.data.selected = _.union($scope.data.ingredients, $scope.data.favorites);
-      }
-    };
-
     var getFavorites = () => {
       Favorites.getFavorites().then((res) => {
-        $scope.favorites = res.data.local.favorites;
-        console.log($scope.favorites);
-        //$scope.data.favorites = $scope.data.favorites;
+        $scope.data.favorites = res.data.local.favorites;
       });
     };
-    getFavorites();
-
     /**
      * @name addIngredients
      * @desc Takes the user input ingredient and adds it to the array of current ingredients
      * @returns undefined
      */
-    $scope.addIngredient = function (ingred) {
+    $scope.selectIngredient = function (ingredient) {
       // Scrub the input string so that it looks standardized. For exmaple, strings such as
       //   'chICKEN        FinGers' -> 'Chicken Fingers'. 
-      var name = !!ingred ? ingred : $scope.ingredient;
+      var name = $scope.ingredient || ingredient;
       name = name.trim().split(/\s+/).map(function(item) {
         return item[0].toUpperCase() + item.substr(1).toLowerCase();
       }).join(' ');
-      $scope.data.ingredients.indexOf(name) === -1 && $scope.data.ingredients.push(name);
+      $scope.data.selected.indexOf(name) === -1 && $scope.data.selected.push(name);
       $scope.message = '';
-      $scope.data.selected = _.union($scope.data.ingredients, $scope.data.favorites);
-      console.log($scope.data.selected);
       $scope.ingredient = '';
-      //$scope.data.selected = $scope.data.favorites.concat($scope.data.ingredients);
     };
 
     $scope.selectFavorite = (favorite) => {
-      console.log('Clicked');
-        console.log(favorite);
-      if ($scope.data.favorites.indexOf(favorite) === -1) {
+      if ($scope.data.selected.indexOf(favorite) === -1) {
         $scope.data.selected.push(favorite);
       }
     };
 
-    $scope.selectIngredient = function(ingredient){
-      $scope.addFavorite(ingredient);
-    }
-
     $scope.addFavorite = function(favorite) {
-      var name = !!favorite ? favorite : $scope.ingredient;
+      var name = $scope.ingredient || favorite;
       name = name.trim().split(/\s+/).map(function(item) {
         return item[0].toUpperCase() + item.substr(1).toLowerCase();
       }).join(' ');
+
       if ($scope.data.favorites.indexOf(name) === -1) {
-        console.log("name",name);
         Favorites.addFavorite(name).then((resp) => {
           getFavorites();
         });
       }
       $scope.ingredient = '';
     };
-
-    // $scope.addFavorite = () => {
-    //   // used to demo shared object between components.
-    //   $scope.shared.input = $scope.data.favorite;
-    //   console.log('Clicked');
-
-    //   if ($scope.data.favorite && $scope.data.favorites.indexOf($scope.data.favorite) === -1) {
-    //     var name = $scope.data.favorite.trim().split(/\s+/).map(function(item) {
-    //       return item[0].toUpperCase() + item.substr(1).toLowerCase();
-    //     }).join(' ');
-    //     if ($scope.data.favorites.indexOf(name) === -1) {
-    //       Favorites.addFavorite(name).then(() => {
-    //         $scope.data.favorite = '';
-    //         getFavorites();
-    //       });
-    //     }
-    //   }
-    // };
 
     $scope.removeFavorite = (favorite) => {
       Favorites.removeFavorite(favorite).then(() => {
@@ -99,8 +59,9 @@ angular.module('fridegly.search', [])
      *       ingredient fron the list
      * @returns undefined
      */
-    $scope.deleteIngredient = function () {
-      $scope.data.selected.splice($scope.data.ingredients.indexOf(this.ingredient), 1);
+    $scope.deleteIngredient = function(ingredient) {
+      var ingredient = ingredient || this.ingredient;
+      $scope.data.selected.splice($scope.data.selected.indexOf(ingredient), 1);
     };
 
     /**
@@ -119,6 +80,8 @@ angular.module('fridegly.search', [])
         Search.sendIngredients(ingredients);
       }
     };
+
+    getFavorites();
 
     Search.getIngredientList()
       .then(function(res){
